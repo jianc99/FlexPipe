46b63f88c566442b05a927732a4a3e58cfae0815
jianc99 <jianchen0311@gmail.com>
2024-04-25 22:06:44 -0400

    Update llm_dist_benchmark.sh

--- llm_dist_benchmark.sh
+++ llm_dist_benchmark.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 \
+CUDA_VISIBLE_DEVICES=0,1 \
 OMP_NUM_THREADS=48 \
-torchrun --nproc_per_node=4 llm_dist_benchmark.py
+torchrun --nproc_per_node=2 llm_dist_benchmark.py

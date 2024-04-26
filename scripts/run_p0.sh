6a2c570d52c873b5f5443b28cdd2dd33ebfb6be2
jianc99 <jianchen0311@gmail.com>
2024-04-26 02:25:10 -0400

    uodate

--- scripts/run_p0.sh
+++ scripts/run_p0.sh
@@ -4,4 +4,4 @@ export WORLD_SIZE=3
 export RANK=0
 export NCCL_P2P_DISABLE=1
 
-CUDA_VISIBLE_DEVICES=0 python3 pp.py
+CUDA_VISIBLE_DEVICES=0 python3 cross_node_test.py
--- scripts/run_p1.sh
+++ scripts/run_p1.sh
@@ -4,4 +4,4 @@ export WORLD_SIZE=3
 export RANK=1
 export NCCL_P2P_DISABLE=1
 
-CUDA_VISIBLE_DEVICES=1 python3 pp.py
+CUDA_VISIBLE_DEVICES=0 python3 cross_node_test.py

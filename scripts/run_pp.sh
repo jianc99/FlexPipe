030db1c97101ff93a432c481f03978e019e67125
jianc99 <jianchen0311@gmail.com>
2024-05-05 15:46:15 -0400

    Update run_pp.sh

--- scripts/run_pp.sh
+++ scripts/run_pp.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 \
+CUDA_VISIBLE_DEVICES=0,1 \
 OMP_NUM_THREADS=48 \
-torchrun --nproc_per_node=4 pp_benchmark.py --layer_partition 32 --tp_groups 4
+torchrun --nproc_per_node=2 pp_benchmark.py --layer_partition 32 --tp_groups 2

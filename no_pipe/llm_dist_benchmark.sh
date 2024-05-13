6eba2f172ffd5749c5837e86595c16f322cad7c2
jianc99 <jianchen0311@gmail.com>
2024-05-13 01:29:28 -0400

    update

--- no_pipe/llm_dist_benchmark.sh
+++ no_pipe/llm_dist_benchmark.sh
@@ -1,3 +1,3 @@
-CUDA_VISIBLE_DEVICES=0,1 \
-OMP_NUM_THREADS=48 \
+export CUDA_VISIBLE_DEVICES=0,1
+export OMP_NUM_THREADS=48
 torchrun --nproc_per_node=2 llm_dist_benchmark.py
--- scripts/run_pp.sh
+++ scripts/run_pp.sh
@@ -1,3 +1,3 @@
-export CUDA_VISIBLE_DEVICES=5,6,7,8
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 export OMP_NUM_THREADS=48
 torchrun --nproc_per_node=4 autoregressive_inference.py --layer_partition 80 --tp_groups 4

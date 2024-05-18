4217b1120fca77079c43266c15ac18a5566421d2
jianc99 <jianchen0311@gmail.com>
2024-05-18 01:12:40 -0400

    update

--- scripts/run_pp.sh
+++ scripts/run_pp.sh
@@ -1,3 +1,3 @@
-export CUDA_VISIBLE_DEVICES=2,3,4,5
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export OMP_NUM_THREADS=48
-torchrun --nproc_per_node=4 batchsize_latency_plot.py --layer_partition 80 --tp_groups 4
+torchrun --nproc_per_node=8 pp_benchmark.py --layer_partition 80 --tp_groups 8

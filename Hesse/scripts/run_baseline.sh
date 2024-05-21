be794a0d15fa556162ceba5f3acd26bf97e7dea6
jianc99 <jianchen0311@gmail.com>
2024-05-20 22:13:33 -0400

    Update run_baseline.sh

--- Hesse/scripts/run_baseline.sh
+++ Hesse/scripts/run_baseline.sh
@@ -1,3 +1,3 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export OMP_NUM_THREADS=48
-torchrun --nproc_per_node=8 tests/baseline_benchmark.py --layer_partition 32 --tp_groups 8
+torchrun --nproc_per_node=8 tests/baseline_benchmark.py --layer_partition 80 --tp_groups 8
